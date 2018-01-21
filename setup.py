a79a09bd6b95abbf6ffe9fa7e60f5a62a7d323a9
angrypig7 <angrypig777@gmail.com>
2018-01-21 19:43:03 +0900

    first upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+#
+# used to convert to win executable
+#
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "console"
+
+setup(  name = "BenEduAutomatedScript",
+    version = "1.0.2",
+    description = "Automate BenEdu!",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("Source.py", base = base)])
